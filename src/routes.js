69f051b4b3733e907cfef73410d5d20ffc7874d9
Rafael Santos <rf.sants001@gmail.com>
2025-03-18 17:09:20 -0300

    feat ✏️: Adicionar controle de categorias e integração com o repositório de categorias

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const ContactController = require("./controllers/ContactController");
+const CategoryController = require("./controllers/CategoryController");
 
 const router = express();
 
@@ -8,5 +9,8 @@ router.get("/contacts/:id", ContactController.show);
 router.delete("/contacts/:id", ContactController.delete);
 router.post("/contacts", ContactController.store);
 router.put("/contacts/:id", ContactController.update);
+
+router.get("/categories", CategoryController.index);
+router.post("/categories", CategoryController.store);
 
 module.exports = router;
